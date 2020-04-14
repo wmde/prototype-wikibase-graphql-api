cf4a663c3807d350a1585b3810a616f2362d50bc
Jakob Warkotsch <jakob.warkotsch@wikimedia.de>
2020-04-14 20:23:19 +0200

    Add initial claims resolver and schema

--- src/types/item.schema.js
+++ src/types/item.schema.js
@@ -13,13 +13,54 @@ const typeDefs = gql`
     ## Implement those later
     # descriptions: [Description]
     # aliases: [Alias]]
-    # claims: [Claim]
+    claims: [Claim]
     # sitelinks: [Sitelink]
   }
 
   type Label {
       language: String!
       value: String!
+  }
+
+  type Claim {
+    id: String!
+    mainsnak: Snak!
+  }
+
+  interface Snak {
+    property: String! # We probably want this to be a Property object and not a string at some point
+    snaktype: String!
+    datatype: String!
+  }
+
+  type PropertyValueSnak implements Snak {
+    property: String! # We probably want this to be a Property object and not a string at some point
+    snaktype: String!
+    datatype: String!
+
+    datavalue: Value!
+  }
+
+  union Value = Item | StringValue | UnknownValue
+
+  type StringValue {
+    value: String!
+  }
+
+  type UnknownValue { # This value type doesn't actually exist and acts as a fallback for unknown/not yet implemented values
+    value: String
+  }
+
+  type PropertyNoValueSnak implements Snak {
+    property: String! # We probably want this to be a Property object and not a string at some point
+    snaktype: String!
+    datatype: String!
+  }
+
+  type PropertySomeValueSnak implements Snak {
+    property: String! # We probably want this to be a Property object and not a string at some point
+    snaktype: String!
+    datatype: String!
   }
 
   type Query {
