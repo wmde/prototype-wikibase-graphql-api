c43a9295601d70e2c0876fa1a710217e62375bea
Jakob Warkotsch <jakob.warkotsch@wikimedia.de>
2020-04-20 17:28:19 +0200

    Ensure the item query succeeded before displaying the label

--- client-demo/src/components/HelloWorld.vue
+++ client-demo/src/components/HelloWorld.vue
@@ -1,8 +1,8 @@
 <template>
   <div>
       <input v-model.lazy="itemId"/>
-      <div v-if="itemId">
-          label: <strong>{{item.labels[0].value}}</strong>
+      <div v-if="itemLabel">
+          label: <strong>{{ itemLabel }}</strong>
       </div>
   </div>
 </template>
@@ -14,11 +14,12 @@ export default {
   data() {
     return {
       itemId: '',
+      itemLabel: ''
     }
   },
 
   apollo: {
-    item: {
+    itemLabel: {
       query: gql`query item($itemId: String!){
         item(id: $itemId) {
           id
@@ -34,6 +35,9 @@ export default {
       },
       skip() {
         return !this.itemId
+      },
+      update(result) {
+        return result.item.labels[0].value
       }
 
     }
